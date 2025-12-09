8389282826dfbaffa5b7a179e54b846441139c47
SiDe1991 <37549209+SiDe1991@users.noreply.github.com>
2025-12-09 13:51:37 +0000

    Step 4

--- octofit-tracker/backend/octofit_tracker/admin.py
+++ octofit-tracker/backend/octofit_tracker/admin.py
@@ -3,8 +3,11 @@ from django.contrib.auth import get_user_model
 from .models import Team, Activity, Leaderboard, Workout
 
 User = get_user_model()
-
-admin.site.register(User)
+from django.contrib.admin.sites import AlreadyRegistered
+try:
+	admin.site.register(User)
+except AlreadyRegistered:
+	pass
 admin.site.register(Team)
 admin.site.register(Activity)
 admin.site.register(Leaderboard)
--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-%$7p!6zdiu%$awt7md0-x$xm!!4!+we!m-j8m49th7x!q19p=)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
